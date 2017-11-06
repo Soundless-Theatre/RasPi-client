29ed14da3dc3c3efb8634dc4b89313b45d5d24bf
yuto <yuto@Let-Debi.local>
2017-11-06 17:16:08 +0900

    websocketで実装

--- recv.py
+++ recv.py
@@ -1,15 +1,6 @@
-from socket import *
-import sys 
-import playaudio
-HOST = ""
-PORT = 5008
-
-s = socket(AF_INET,SOCK_DGRAM)
-s.bind((HOST,PORT))
-
+import plaaudio
+from websocket import create_connection
+ws = create_connection("ws://localhost:9999")
 while True:
-   msg, address = s.recvfrom(8192)
-   playaudio.play(msg)
-
-s.close()
-sys.exit()
+    playaudio.play(ws.recv())
+ws.close()
