c27bd485a5e9334c3d7f66907d99cf61a42ac68f
YutoTamura <s17130@tokyo.kosen-ac.jp>
2018-02-23 13:48:13 +0900

    [add]受信部を別タスクに分けて音質改善を試みる

--- recv.py
+++ recv.py
@@ -1,15 +1,18 @@
 from socket import *
 import sys 
 import playaudio
+from multiprocessing import Process
 HOST = ""
 PORT = 5008
-
 s = socket(AF_INET,SOCK_DGRAM)
 s.bind((HOST,PORT))
 
-while True:
-   msg, address = s.recvfrom(8192)
-   playaudio.play(msg)
-
+if __name__=="__main__":
+    msg, address = s.recvfrom(8192)
+    while True:
+        p=Process(target=(playaudio.play),args=(msg,))
+        p.start()
+        msg, address = s.recvfrom(8192)
+        p.join()
 s.close()
 sys.exit()
