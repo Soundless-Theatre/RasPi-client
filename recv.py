8fddaa23f15ddb8de2b78f63c94644b9e7da9e87
YutoTamura <s17130@tokyo.kosen-ac.jp>
2018-02-26 16:15:32 +0900

    [update]1つのfileにまとめた

--- recv.py
+++ recv.py
@@ -1,7 +1,8 @@
+import pyaudio
 from socket import *
 import sys 
-import playaudio
 from multiprocessing import Process,Queue
+
 HOST = ""
 PORT = 5008
 s = socket(AF_INET,SOCK_DGRAM)
@@ -10,13 +11,23 @@ s.bind((HOST,PORT))
 def recv(q):
     msg,address=s.recvfrom(8192)
     q.put(msg)
+
+CHUNK=128
+RATE=16000
+p=pyaudio.PyAudio()
+stream=p.open( format = pyaudio.paInt16,
+              channels = 2,
+              frames_per_buffer = CHUNK,
+              input = False,
+              output = True)
+
 if __name__=="__main__":
     msg, address = s.recvfrom(8192)
     while True:
         q=Queue()
         p=Process(target=recv,args=(q,))
         p.start()
-        playaudio.play(msg)
+        stream.write(msg)
         msg=q.get()
         p.join()
 s.close()
